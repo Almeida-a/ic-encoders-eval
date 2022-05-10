e50a455ddc2349f838f5de83b288d5d9d5c39f7b
Andre Almeida <aralmeida99@gmail.com>
2022-05-10 17:38:04 +0100

    Finished implementing JPEG evaluation script

--- jpeg_eval.py
+++ jpeg_eval.py
@@ -1,6 +1,5 @@
-""" Goal is to map the JPEG CR into an MSSIM value
+""" Goal is to map the JPEG CR into MSSIM/PSNR values
 
-Deprecate?
 This is a project side-experiment.
 
 """
@@ -14,19 +13,20 @@ import numpy as np
 import pandas as pd
 
 import metrics
+import parameters
 from parameters import JPEG_EVAL_RESULTS_FILE
 
-QUALITY_STEPS: int = 20
+QUALITY_SPREAD: int = 20
 # Quality settings
-QUALITY_VALUES: np.ndarray = np.linspace(1, 100, QUALITY_STEPS)
+QUALITY_VALUES: np.ndarray = np.linspace(1, 100, QUALITY_SPREAD)
 # Where the raw/processed results of the experiment are written on
 
 
 def images(ext: str) -> str:
-    """
-    Generator function
+    """Generator function that yields images to be studied
 
-    :return: Sequence of image files path to be processed in experience
+    :param ext: Extension used to filter the images (the ones that don't have that format)
+    :return: Sequence of image files path to be processed for the experiment
     """
 
     possible_ext = (".jpg", ".png")
@@ -42,64 +42,73 @@ def images(ext: str) -> str:
 
 
 def compress_n_compare():
+    """ Compress and extract jpeg statistic
 
-    """
     The purpose is to compress a set of uncompressed images in JPEG
     Compression is performed using multiple quality configurations
-    For each quality configuration, compute the SSIM of the resulting image (versus the dataset)
+    For each quality configuration, compute the metrics of the resulting image (CR, PSNR, SSIM versus the dataset)
 
     :return:
     """
 
-    # Extension of the images to be processed
-    ext: str = ".png"
-
     # Compress using the above quality parameters
     # Save the compression ratio in a dataframe
-    df = pd.DataFrame(data=dict(fname=[], original_size=[], compressed_size=[], CR=[], SSIM=[]))
-    for file_path in images(ext):
-        file_name: str = ".".join(file_path.split("/")[-1].split(".")[:-1])
+    df = pd.DataFrame(data=dict(fname=[], cr=[], mse=[], psnr=[], ssim=[]))
+    for file_path in images(parameters.LOSSLESS_EXTENSION):
+        file_name: str = ".".join(os.path.basename(file_path).split(".")[:-1])
         for quality in QUALITY_VALUES:
             quality = int(quality)
 
             # Read input file
-            img = cv2.imread(file_path)
+            img = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)
 
-            # Compress input file
-            comp_img = cv2.imencode(".jpg", img, [cv2.IMWRITE_JPEG_QUALITY, quality])
-            comp_img_bytes: bytes = np.array(comp_img[1]).tobytes()
+            # Truncate the image to 8 bits, since jpeg can't encode above 8 bits
+            img = img.astype(np.uint8)
+
+            # Encode input file
+            status, img_encode = cv2.imencode('.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, quality])
+            assert status is True, f"Error encoding the image \"{file_path}\""
+
+            # Get the compressed image size
+            assert img_encode.dtype == np.uint8, f"JPEG bitstream dtype should be uint8, not {img_encode.dtype}!"
+            bit_depth = 8
+            comp_img_size: int = img_encode.size * bit_depth
 
             # Calculate dataset_compressed bitstream size
-            cr = int(img.size / (len(comp_img_bytes) * 8))
+            og_image_bitdepth = 8
+            cr = int(img.size * og_image_bitdepth / comp_img_size)
 
             # Decode JPEG bitstream
-            buffer: np.array = np.asarray(bytearray(comp_img_bytes), dtype=np.uint8)
-            img_c = cv2.imdecode(buffer, cv2.IMREAD_COLOR)
+            img_comp = cv2.imdecode(np.array(img_encode), cv2.IMREAD_UNCHANGED)
 
             # Calculate the SSIM between the images
-            ssim = metrics.ssim(
-                cv2.cvtColor(img, cv2.COLOR_BGR2GRAY),
-                cv2.cvtColor(img_c, cv2.COLOR_BGR2GRAY)
+            mse, psnr, ssim = (
+                metric(img, img_comp) for metric in (metrics.mse, metrics.psnr, metrics.ssim)
             )
 
             # Write to dataframe
             df = pd.concat([pd.DataFrame(dict(
                 fname=[f"{file_name}_q{quality}"],
-                original_size=[img.size],
-                compressed_size=[len(comp_img_bytes) * 8],
-                CR=[cr],
-                SSIM=ssim
-            )), df], ignore_index=True)
-    df.to_csv("results.csv", index=False)
+                cr=[cr],
+                mse=[mse],
+                psnr=[psnr],
+                ssim=[ssim]
+            )), df])
+    df.to_csv(f"{JPEG_EVAL_RESULTS_FILE}.csv", index=False)
 
 
-def comparison_stats(csv_file_path: str):
+def squeeze_stats(csv_file_path: str):
+    """Reads csv with raw data and writes json w/ more readable statistics
+
+    :param csv_file_path: File with raw data
+    :return:
+    """
     # Check validity of argument
     if not os.path.exists(csv_file_path):
         raise AssertionError(f"Path: {csv_file_path} does not contain a file!")
     if not csv_file_path.endswith(".csv"):
         raise AssertionError(f"Argument file {csv_file_path} format is not csv")
-    # Read csv
+
     df = pd.read_csv(csv_file_path)
 
     # For common quality settings, calculate stats for CR and SSIM (min/max/avg/dev)
@@ -114,21 +123,20 @@ def comparison_stats(csv_file_path: str):
                 sub_df.loc[len(sub_df.index)] = row
         # Set up new entry in stat dict
         stats[f"q{quality_val}"] = dict()
-        # Write stats in dict
-        cr_list: List[float] = sub_df["CR"]
-        stats[f"q{quality_val}"]["CR"] = {
-            "min": min(cr_list), "max": max(cr_list), "avg": np.mean(cr_list), "dev": np.std(cr_list)
-        }
-        ssim_list: List[float] = sub_df["SSIM"]
-        stats[f"q{quality_val}"]["SSIM"] = {
-            "min": min(ssim_list), "max": max(ssim_list), "avg": np.mean(ssim_list), "dev": np.std(ssim_list)
-        }
+
+        for metric in (*parameters.qmetrics.keys(), "cr"):
+            # Write stats in dict
+            metric_vals: List[float] = sub_df[metric]
+            stats[f"q{quality_val}"][metric] = dict(
+                min=min(metric_vals), max=max(metric_vals),
+                avg=np.mean(metric_vals), stddev=np.std(metric_vals)
+            )
 
     # Store the results in a json file
-    out_file = open(JPEG_EVAL_RESULTS_FILE+".json", "w")
+    out_file = open(f"{JPEG_EVAL_RESULTS_FILE}.json", "w")
     json.dump(stats, out_file, indent=6)
 
 
 if __name__ == '__main__':
-    # compress_n_compare()
-    comparison_stats(JPEG_EVAL_RESULTS_FILE + ".csv")
+    compress_n_compare()
+    squeeze_stats(f"{JPEG_EVAL_RESULTS_FILE}.csv")
