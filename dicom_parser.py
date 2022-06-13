75f7bf92de9dd00f6a77226c20787477a666bf93
Andre Almeida <aralmeida99@gmail.com>
2022-06-13 17:19:41 +0100

    Multi-frame images are now also encoded to apng
     * That is because cjxl doesn't support .tiff as input, but supports .apng (however, cavif-rs only supports jpg/png, thus I, for now, won't be evaluating multi-frame for avif)
     * Also, the program dicom_parser.py deletes all files from images/dataset before writing them to that directory - because I would need to manually delete those images whenever I need to re-run the script over the same images

--- dicom_parser.py
+++ dicom_parser.py
@@ -5,8 +5,9 @@
 """
 
 import os
-from typing import List
+from typing import List, Tuple
 
+import apng
 import cv2
 import numpy as np
 from PIL import Image, ImageSequence
@@ -44,16 +45,15 @@ def parse_dcm(filepath: str):
     body_part = file_data[BODY_PART_TAG]
     bps = file_data[STORED_BITS_TAG]  # bits per sample TODO be careful, since the bpp at the main pipeline is based on
     #                                                      the max pixel value, different from how it is obtained here
-    spp = file_data[SAMPLES_PER_PIXEL_TAG]
+    samples_per_pixel = file_data[SAMPLES_PER_PIXEL_TAG]
     color_space = file_data[PHOTOMETRIC_INTERPRETATION_TAG]
 
     # Read the pixel data
     img_array = file_data.pixel_array
 
     # Set image path where it will be written on
-    out_img_path: str = DATASET_PATH + f"{modality.value.replace(' ', '')}_" \
-                                       f"{body_part.value}_" \
-                                       f"{'_'.join([str(elem.value) for elem in (color_space, spp, bps)])}"
+    attributes = '_'.join([str(elem.value) for elem in (color_space, samples_per_pixel, bps)])
+    out_img_path: str = DATASET_PATH + f"{modality.value.replace(' ', '')}_{body_part.value}_{attributes}"
 
     repetition_id = 0
 
@@ -72,10 +72,12 @@ def parse_dcm(filepath: str):
 
     out_img_path += LOSSLESS_EXTENSION
 
-    if len(img_array.shape) <= 2 or "RGB" in color_space:
+    single_channel: bool = samples_per_pixel.value == 1
+
+    if len(img_array.shape) <= 2 or not single_channel and len(img_array.shape) == 3:
         saved_img_array = write_single_frame(img_array, out_img_path)
     else:
-        saved_img_array = write_multi_frame(img_array, out_img_path)
+        saved_img_array = write_multi_frame(out_img_path, img_array, single_channel)
 
     if (img_array == saved_img_array).all() is False:
         # Remove written image
@@ -84,25 +86,71 @@ def parse_dcm(filepath: str):
         raise AssertionError(f"Quality loss accidentally applied to the image \"{out_img_path}\"!")
 
 
-def write_multi_frame(img_array, out_img_path):
+def to_np_array(apng_img: apng.APNG) -> np.ndarray:
+
+    frames_arr = []
+
+    for frame, control in apng_img.frames:
+        tmp_fname = "tmp.png"
+        frame.save(tmp_fname)
+        frames_arr.append(
+            cv2.imread(tmp_fname, cv2.IMREAD_UNCHANGED)
+        )
+        os.remove(tmp_fname)
+
+    return np.array(frames_arr)
+
+
+def im_write_multi_apng(file_name: str, img_array: np.ndarray) -> Tuple[bool, np.ndarray]:
+
+    sub_frames_fn_list = []
+
+    for i in range(img_array.shape[0]):
+        frame: np.ndarray = img_array[i]
+
+        sub_frame_fname = f"tmp{i}.png"
+        sub_frames_fn_list.append(sub_frame_fname)
+
+        cv2.imwrite(sub_frame_fname, frame, params=[cv2.IMWRITE_TIFF_COMPRESSION, 1])
+
+    apng_img = apng.APNG.from_files(sub_frames_fn_list)
+    apng_img.save(file_name)
+
+    for i in range(img_array.shape[0]):
+        os.remove(f"tmp{i}.png")
+
+    return True, to_np_array(apng_img)
+
+
+def write_multi_frame(out_img_path: str, img_array: np.ndarray, is_single_channel: bool):
     """Write a multi-frame image to the specified path
 
     Warning: his function works for .tiff formats, might not work, however for other formats.
 
+    :param is_single_channel: Boolean flag indicating if image is in rgb colorspace
     :param img_array: Image matrix. Image color-spaces can be supported according to the tiff format
     :param out_img_path: Relative path where the image should be written to
     :return: Matrix structure of the written image
     """
-    out_img_path_tiff = out_img_path.replace(".png", ".tiff")
+
+    # Pre-condition(s)
+    assert is_single_channel and len(img_array.shape) == 3 or\
+           not is_single_channel and len(img_array.shape) == 4, "Image is single frame!"
+
+    out_img_path_tiff, out_img_path_apng = [out_img_path.replace(".png", format_) for format_ in (".tiff", ".apng")]
 
     # Save to tiff
     assert cv2.imwritemulti(out_img_path_tiff, img_array) is True, "Image writing (multi-frame) failed."
     frames: List[Image] = ImageSequence.all_frames(Image.open(out_img_path_tiff))
-    saved_img_array = np.array([np.array(frames[i]) for i in range(img_array.shape[0])])
+    saved_img_array_tiff = np.array([np.array(frames[i]) for i in range(img_array.shape[0])])
 
-    # TODO Save also to apng - One way it to write each frame to png and then aggregate using the "apng" lib
+    status_, saved_img_array_apng = im_write_multi_apng(out_img_path_apng, img_array)
+    assert status_ is True, "Error writing apng image"
 
-    return saved_img_array
+    assert (saved_img_array_tiff == saved_img_array_apng).all(), "Quality loss in either apng " \
+                                                                         "or tiff multi-frame image files."
+
+    return saved_img_array_tiff
 
 
 def write_single_frame(img_array: np.ndarray, out_img_path: str) -> np.ndarray:
@@ -136,6 +184,10 @@ if __name__ == "__main__":
     # Get all dicom files (hardcoded)
     for filename in os.listdir(raw_dataset):
         dirs.append(raw_dataset + filename)
+
+    # Empty the images/dataset directory
+    for file in os.listdir("images/dataset"):
+        os.remove(f"images/dataset/{file}")
 
     # Call a function to parse each dicom file
     for dcm_file in dirs:
