dfbce1bbe1501e5a35bdc4f39faaa295b063ae98
Almeida-a <almeida.a@ua.pt>
2022-07-11 20:01:13 +0100

    feat(visualize_data and dicom_parser): Line graphs for metric values comparizon is fully parametrizable
    We can parametrize which format, modality and metric that we want to
    evaluate
    And replicated changes from #f9ce292

--- visualize_data.py
+++ visualize_data.py
@@ -1,9 +1,10 @@
 import re
-from typing import List
+from typing import List, Callable, Any
 from typing import Optional
 
 import matplotlib.pyplot as plt
 import pandas
+import pandas as pd
 
 from parameters import PROCEDURE_RESULTS_FILE
 
@@ -15,6 +16,8 @@ from parameters import PROCEDURE_RESULTS_FILE
         fixed variables -> modality, compression format
         
 """
+
+
 # TODO either produce a function for each case or implement a master
 #  function (an intermediate solution can also be creating functions
 #  for each case but they all call over a private master function.
@@ -23,10 +26,12 @@ from parameters import PROCEDURE_RESULTS_FILE
 
 def draw_graph(x: List[float], y: List[float],
                x_label: str = "", y_label: str = "", title: str = ""):
-    """
+    """Draws a graph given a list of x and y values
 
-    :param x:
-    :param y:
+    TODO add parameter to define type of graph (line, histogram, ...)
+
+    :param x: Independent axis vector
+    :param y: Dependent axis vector
     :param x_label:
     :param y_label:
     :param title:
@@ -51,36 +56,55 @@ def draw_graph(x: List[float], y: List[float],
     plt.show()
 
 
-def ssim_per_quality(modality: str,
-                     raw_data_fname: str = PROCEDURE_RESULTS_FILE+".csv",
+def search_dataframe(df: pd.DataFrame, key: str, value: str) -> pd.DataFrame:
+    """Search the dataframe for rows containing particular attribute value
+
+    @param df: Dataframe to be searched over
+    @param key: Attribute key element
+    @param value: Attribute value
+    @return: All rows matching the attribute value
+    """
+    return df.loc[df[key] == value]
+
+
+def metric_per_image(modality: str, metric: str,
+                     raw_data_fname: str = PROCEDURE_RESULTS_FILE + ".csv",
                      compression_format: str = "jxl"):
-    # Make sure the modality is uppercase
+    """Given a data file, display statistics of kind metric = f(quality)
+
+    Line graph portraying metric = f(quality), given the modality and format
+
+    @param raw_data_fname: input data - file containing the procedure results
+    @param modality: Used to filter the global data
+    @param compression_format: Used to filter the global data
+    @param metric: Evaluates a characteristic of a file compression (e.g.: ratio, similarity, speed)
+    """
+    assert raw_data_fname.endswith(".csv"), f"Data source must be a csv file! Found \"{raw_data_fname}\"."
+
     modality = modality.upper()
 
     # Read file into dataframe
-    df = pandas.read_csv(raw_data_fname)
+    df: pd.DataFrame = pandas.read_csv(raw_data_fname)
 
     # Get the file names w/ the given modality, format and effort/speed value
-    # Use regex
-    filtered_fnames_x: List[Optional[str]] = list()
-    for filename_x in df["filename"]:
+    filtered_fnames_x = list(filter(
+        lambda filename: re.fullmatch(
+            pattern=rf"{modality}_[\w_]+q\d+(.\d+)?-e\d.{compression_format}",
+            string=filename
+        ) is not None,
+        tuple(df.filename)
+    ))
 
-        match = re.fullmatch(
-            pattern=rf"{modality}_[a-zA-Z]+_(\d+_)?q\d+(.\d+)?-e\d.{compression_format}",
-            string=filename_x
-        )
-
-        if match is not None:
-            filtered_fnames_x.append(match.string)
-
-    # Extract y list: ssim per fname in filtered_fnames
-    ssim_y = list()
-    for filename_x in filtered_fnames_x:
-        ssim_y.append(float(df.loc[df["filename"] == filename_x]["ssim"]))
+    ssim_y = [
+        float(search_dataframe(df, key="filename", value=filename)[metric].values[0])
+        for filename in filtered_fnames_x
+    ]
 
     # Draw graph
-    draw_graph(x=list(range(len(ssim_y))), y=ssim_y)
+    draw_graph(x=list(range(len(ssim_y))), y=ssim_y, y_label=metric, title=f"Modality: {modality},"
+                                                                           f" Format: {compression_format}")
 
 
 if __name__ == '__main__':
-    ssim_per_quality(modality="CT")
+    metric_per_image(modality="CT", metric="cr")  # for now, displays a line graph
+    # TODO use histogram - quality setting for each bar
